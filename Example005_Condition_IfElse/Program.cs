381e1ae8aaf4c0c36e9a3fce8d09138428eceee6
Olga <frau.tsaregorodtseva2016@gmail.com>
2023-01-21 14:01:19 +0300

    Example_005_IfElse

---
+++ Example005_Condition_IfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string? name = Console.ReadLine();
+
+if(name.ToLower() == "маша")
+{
+   Console.WriteLine("Ура, это же Маша!");
+}
+
+else
+{
+   Console.Write("Привет," + name);
+   //Console.WriteLine(username);
+}
