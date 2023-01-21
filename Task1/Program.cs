e20f8682d911aa48ddee49c4d1dff35e557e3eaf
Olga <frau.tsaregorodtseva2016@gmail.com>
2023-01-21 23:07:04 +0300

    Task1_ne kvadrat

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+System.Console.Write("Первое число: ");
+int a =Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Второе число: ");
+int b =Convert.ToInt32(Console.ReadLine());
+
+if (a == (b * b))
+{
+    Console.WriteLine($"{a} квадрат {b}");
+}
+else
+{
+    System.Console.WriteLine("Не квадрат!");
+}
