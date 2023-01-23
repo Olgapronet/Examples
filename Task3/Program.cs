a8ff95e66f89acce1906fffc98342bee827a93cf
Olga <frau.tsaregorodtseva2016@gmail.com>
2023-01-23 11:23:38 +0300

    Task3_Последняя цифра

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.Write("--> ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if ( (a < 1000) && (a > 99) ) {
+    int b = a % 10;
+    System.Console.WriteLine(b);
+}
+
+else
+{
+    System.Console.WriteLine("Ошибка");
+}
